36fb00fcced40fa41dfcc91964afb278036c969d
Damian Bertazza <damian.bertazza@gmail.com>
2021-09-30 13:48:57 -0300

    Ejercicio websocket 2

--- Clase-13_Websocket2/public/index.js
+++ Clase-13_Websocket2/public/index.js
@@ -13,6 +13,8 @@ saveProduct.addEventListener("submit", e => {
 });
 
 socket.on('catalogo', data => {
+    console.log('catalogo', data)
+
     const dynamicTable = document.getElementById("dynamic-table");
     dynamicTable.innerHTML = '';
     if (dynamicTable && data.length > 0) {
@@ -48,5 +50,39 @@ socket.on('catalogo', data => {
         </div>
         `
         dynamicTable.innerHTML = emptyMsg;
+    }
+});
+
+socket.on('chat', data => {
+    console.log(data)
+    const chatBox = document.getElementById("chat-box");
+    if ( chatBox && data.length > 0 ) {
+        chatBox.innerHTML = '';
+        data.forEach(element => {
+            const msgWrapper = document.createElement("div");
+            msgWrapper.innerHTML = `
+                <strong class="text-primary">${element.email} </strong><span class="text-danger">[${element.date}]: </span><em class="text-success">${element.text}</em>
+            `
+            chatBox.appendChild(msgWrapper);
+        });
+    }
+});
+
+const chatForm = document.getElementById("chat-form");
+chatForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const email = document.getElementById("chat-email").value;
+    const msg = document.getElementById("chat-msg").value;
+    const today = new Date();
+    const dateTime = `${today.getDate()}/${today.getMonth()+1}/${today.getFullYear()} - ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+    if (!email || !msg ) {
+        alert("Debe completar los campos antes de enviar un mensaje")
+    } else {
+        const chatMsg = {
+            email: email,
+            date: dateTime,
+            text: msg
+        }
+        socket.emit('chatMsg', chatMsg)
     }
 });
