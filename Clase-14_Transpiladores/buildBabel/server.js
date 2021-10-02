a44d4b051e47bd7b970b73a81086dcffd6d1dba4
Damian Bertazza <damian.bertazza@gmail.com>
2021-10-02 12:44:39 -0300

    update para babel y ts;

--- Clase-14_Transpiladores/buildBabel/server.js
+++ Clase-14_Transpiladores/buildBabel/server.js
@@ -1,15 +1,23 @@
+'use strict';
+
 /**/
 var express = require('express');
 var handlebars = require('express-handlebars');
 var session = require('express-session');
 var fs = require('fs');
-var HttpServer = require('http').Server;
-var IOServer = require('socket.io').Server;
+
+var _require = require('http'),
+    HttpServer = _require.Server;
+
+var _require2 = require('socket.io'),
+    IOServer = _require2.Server;
+
 var app = express();
 var PORT = 8080;
 var router = express.Router();
 var httpServer = new HttpServer(app);
 var io = new IOServer(httpServer);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
@@ -17,7 +25,9 @@ app.use(session({
     resave: false,
     saveUninitialized: false
 }));
+
 app.use('/api', router);
+
 // Config de handlebars
 app.engine("hbs", handlebars({
     extname: ".hbs",
@@ -25,45 +35,52 @@ app.engine("hbs", handlebars({
     layoutsDir: __dirname + "/views/layouts/",
     partialsDir: __dirname + "/views/partials/"
 }));
+
 app.set("view engine", "hbs");
 app.set("views", "./views");
+
 app.use(express.static('public'));
+
 httpServer.listen(PORT, function () {
     console.log('Servidor HTTP escuchando en el puerto', PORT);
 });
+
 var products = [];
+
 app.get("/", function (req, res) {
     try {
         var savedProduct = req.session.savedProduct;
         res.render("main", { savedProduct: savedProduct });
         req.session.savedProduct = false;
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error);
     }
 });
+
 router.get('/productos/vista', function (req, res) {
     try {
         if (products.length > 0) {
             req.session.productsAvailable = true;
         }
         res.render("products", { products: products, productsAvailable: req.session.productsAvailable });
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error);
     }
 });
+
 router.post('/productos/listar/id', function (req, res) {
     try {
-        var body_1 = req.body;
-        var product = products.find(function (p) { return p.id == body_1.id; });
+        var body = req.body;
+        var product = products.find(function (p) {
+            return p.id == body.id;
+        });
         req.session.product = product;
-        res.redirect("/api/productos/listar/" + body_1.id);
-    }
-    catch (error) {
+        res.redirect('/api/productos/listar/' + body.id);
+    } catch (error) {
         console.log(error);
     }
 });
+
 router.get('/productos/listar/:id', function (req, res) {
     try {
         if (products.length > 0) {
@@ -71,11 +88,11 @@ router.get('/productos/listar/:id', function (req, res) {
         }
         var product = req.session.product;
         res.render("products", { product: product, productsAvailable: req.session.productsAvailable });
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error);
     }
 });
+
 router.post('/productos/guardar', function (req, res) {
     try {
         var body = req.body;
@@ -86,68 +103,80 @@ router.post('/productos/guardar', function (req, res) {
             error.msg = 'No se puede guardar el producto. Revise los datos proporcionados.';
             res.status(400);
             res.json({ error: error });
-        }
-        else {
+        } else {
             body.id = products.length + 1;
             products.push(body);
             req.session.savedProduct = true;
             res.redirect('/');
         }
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error);
     }
 });
+
 router.put('/productos/actualizar/:id', function (req, res) {
-    var _a = req.body, title = _a.title, price = _a.price, thumbnail = _a.thumbnail;
+    var _req$body = req.body,
+        title = _req$body.title,
+        price = _req$body.price,
+        thumbnail = _req$body.thumbnail;
+
     var params = req.params;
-    var product = products.find(function (p) { return p.id == params.id; });
+    var product = products.find(function (p) {
+        return p.id == params.id;
+    });
+
     if (product && (title !== '' || price !== '' || price > 0 || thumbnail !== '')) {
         product.title = title;
         product.price = price;
         product.thumbnail = thumbnail;
         res.json(product);
-    }
-    else {
+    } else {
         res.status(404);
         res.json({ error: 'Producto no encontrado' });
     }
 });
-router["delete"]('/productos/borrar/:id', function (req, res) {
+
+router.delete('/productos/borrar/:id', function (req, res) {
     var params = req.params;
-    var product = products.find(function (p) { return p.id == params.id; });
+    var product = products.find(function (p) {
+        return p.id == params.id;
+    });
     if (product) {
         products = products.filter(function (x) {
             return x.id != params.id;
         });
         res.json(product);
-    }
-    else {
+    } else {
         res.status(404);
         res.json({ error: 'Producto no encontrado' });
     }
 });
+
 // WEBSOCKETS
 io.on('connection', function (socket) {
     console.log('Usuario Conectado');
+
     socket.emit('catalogo', products);
+
     socket.on('savedProduct', function (data) {
         data.id = products.length + 1;
         products.push(data);
         io.sockets.emit('catalogo', products);
     });
+
     var chats = {
         messages: []
     };
-    fs.readFile(__dirname + "/logs/chat.json", 'utf8', function (err, data) {
+    fs.readFile(__dirname + '/logs/chat.json', 'utf8', function (err, data) {
         if (!err) {
             chats.messages = JSON.parse(data).messages;
             socket.emit('chat', chats.messages);
         }
     });
+
     socket.on('chatMsg', function (data) {
         chats.messages.push(data);
-        fs.writeFile(__dirname + "/logs/chat.json", JSON.stringify(chats, null, "\t"), 'utf8', function (err, data) {
+        fs.writeFile(__dirname + '/logs/chat.json', JSON.stringify(chats, null, "\t"), 'utf8', function (err, data) {
             if (!err) {
                 io.sockets.emit('chat', chats.messages);
             }
---
+++ Clase-14_Transpiladores/server.es6.js
@@ -0,0 +1,172 @@
+/**/ 
+const express = require('express');
+const handlebars = require('express-handlebars');
+const session = require('express-session');
+const fs = require('fs');
+const { Server: HttpServer } = require('http');
+const { Server: IOServer } = require('socket.io');
+
+const app = express();
+const PORT = 8080;
+const router = express.Router();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(session({
+    secret: 'mySecret', 
+    resave: false, 
+    saveUninitialized: false
+}))
+
+app.use('/api', router);
+
+// Config de handlebars
+app.engine(
+    "hbs",
+    handlebars({
+        extname: ".hbs",
+        defaultLayout: "index.hbs",
+        layoutsDir: __dirname + "/views/layouts/",
+        partialsDir: __dirname + "/views/partials/"
+    })
+);
+
+app.set("view engine", "hbs");
+app.set("views", "./views");
+
+app.use(express.static('public'));
+
+httpServer.listen(PORT, ()=>{
+    console.log('Servidor HTTP escuchando en el puerto', PORT);
+});
+
+let products = [];
+
+app.get("/", (req, res) => {
+    try {
+        const savedProduct = req.session.savedProduct;
+        res.render("main", { savedProduct })
+        req.session.savedProduct = false;
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get('/productos/vista', (req,res)=>{
+    try {
+        if (products.length > 0) {
+            req.session.productsAvailable = true;
+        }
+        res.render("products", { products, productsAvailable: req.session.productsAvailable })
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+router.post('/productos/listar/id', (req,res) => {
+    try {
+        let body = req.body;
+        let product = products.find( p => p.id == body.id );
+        req.session.product = product;
+        res.redirect(`/api/productos/listar/${body.id}`);
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.get('/productos/listar/:id', (req,res)=>{
+    try {
+        if (products.length > 0) {
+            req.session.productsAvailable = true;
+        }
+        const product = req.session.product;
+        res.render("products", { product, productsAvailable: req.session.productsAvailable })
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+router.post('/productos/guardar', (req,res) => {
+    try {
+        let body = req.body;
+        let error = {
+            msg: ''
+        }
+        if ( body.title == '' || body.price == '' || body.price <= 0 || body.thumbnail == '' ) {
+            error.msg = 'No se puede guardar el producto. Revise los datos proporcionados.';
+            res.status(400)
+            res.json({error})
+        } else {
+            body.id = products.length + 1;
+            products.push(body);
+            req.session.savedProduct = true;
+            res.redirect('/');
+        }
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+router.put('/productos/actualizar/:id', (req, res) => {
+    let { title, price, thumbnail } = req.body;
+    let params = req.params;
+    let product = products.find( p => p.id == params.id );
+
+    if (product && (title !== '' || price !== '' || price > 0 || thumbnail !== '')) {
+        product.title = title;
+        product.price = price;
+        product.thumbnail = thumbnail;
+        res.json(product);
+    } else {
+        res.status(404)
+        res.json({error: 'Producto no encontrado'});
+    }
+})
+
+router.delete('/productos/borrar/:id', (req, res) => {
+    let params = req.params;
+    let product = products.find( p => p.id == params.id );
+    if ( product ) {
+        products = products.filter(x => {
+            return x.id != params.id;
+        })
+        res.json(product);
+    } else {
+        res.status(404)
+        res.json({error: 'Producto no encontrado'});
+    }  
+})
+
+// WEBSOCKETS
+io.on('connection', (socket) => {
+    console.log('Usuario Conectado');
+
+    socket.emit('catalogo', products);
+
+    socket.on('savedProduct', (data) => {
+        data.id = products.length + 1;
+        products.push(data)
+        io.sockets.emit('catalogo', products);
+    });
+
+    let chats = {
+        messages: []
+    };
+    fs.readFile(`${__dirname}/logs/chat.json`, 'utf8', (err, data) => {
+        if (!err) {
+            chats.messages = JSON.parse(data).messages;
+            socket.emit('chat', chats.messages)
+        }
+    });
+    
+    socket.on('chatMsg', (data) => {
+        chats.messages.push(data);
+        fs.writeFile(`${__dirname}/logs/chat.json`, JSON.stringify(chats, null, "\t"), 'utf8', (err, data) => {
+            if (!err) {
+                io.sockets.emit('chat', chats.messages);
+            }
+        });
+    });
+});
